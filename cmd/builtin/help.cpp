f552d827f6fac53d8f627b0bd307a20f3dbce6f3
JColeby <jonathan.2022jatc@gmail.com>
2025-12-15 17:35:40 -0700

    updated the help command to give additional info about piping and redirection

--- cmd/builtin/help.cpp
+++ cmd/builtin/help.cpp
@@ -11,7 +11,17 @@ int helpMain(vector<string> tokenizedInput, HANDLE readHandle, HANDLE writeHandl
     helpText << WHITE << "  - press ctrl+k to terminate any active command (may result in memory leaks)" << endl;
     helpText << "  - run 'exit' to close the shell" << endl;
     helpText << "  - run 'cmd' to view a list of all commands" << endl;
-    helpText << "  - run 'man <command>' to view additional info about a specific command" << RESET_TEXT << endl;
+    helpText << "  - run 'man <command>' to view additional info about a specific command" << endl;
+    helpText << "  - to redirect console output to a file, add '<redirectMode> <filepath>' to the end of your command" << endl;
+    helpText << "      - '>' will write the contents of stdout to the file" << endl;
+    helpText << "      - '>>' will append the contents of stdout to the file " << endl;
+    helpText << "      - '2>' will write the contents of stderr to the file" << endl;
+    helpText << "      - '2>>' will append the contents of stderr to the file " << endl;
+    helpText << "      - '&>' will write the contents of stdout and stderr to the file " << endl;
+    helpText << "      - '&>>' will append the contents of stdout and stderr to the file " << endl;
+    helpText << "  - a pipe '|' can be used to redirect the output of one command into the input of another" << endl;
+    helpText << "  - to pass the result of 1 command as a parameter to another one, surround the first command like so: '<cmd2> $(<cmd1>)'" << RESET_TEXT << endl;
+
     string output = helpText.str();
     writeToPipe(writeHandle, output);
     return 0;
